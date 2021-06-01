0f1b4f4bd909101b61a9517a992496d489867f14
Sohee <sohee3480@gmail.com>
2021-06-01 10:12:43 +0900

    Intent 작성해서 다른 화면 로그 출력해보기

--- app/src/main/java/com/example/lifecycle2/MainActivity.kt
+++ app/src/main/java/com/example/lifecycle2/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.lifecycle2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("메인화면", "onCreate 실행됨")
+
+        moveToOtherBtn.setOnClickListener {
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }
     }
 }
